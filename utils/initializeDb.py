b815744d04aa8b64475fdc4c9f488413317e0df4
Bayless <bsmithsalzberg@stuy.edu>
2016-11-03 18:33:24 -0400

    updated initializdb

--- utils/initializeDb.py
+++ utils/initializeDb.py
@@ -8,6 +8,7 @@ f = "database.db"
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
+#------------------------create tables---------------------------------------
 q = "CREATE TABLE users (username TEXT, password TEXT, userId INTEGER)"
 c.execute(q)
 
@@ -17,7 +18,13 @@ c.execute(q)
 q="CREATE TABLE edit_logs (userId INTEGER, storyId INTEGER, time INTEGER)"
 c.execute(q)
 #add random stuff for testing
+#-------------------------------------------------------------------------------
 
-
+#---------------------------put stuff in tables---------------------------------
+q = """INSERT INTO users VALUES ("anya", "hashedpass", 0)"""
+c.execute(q)
+q = """INSERT INTO TABLE stories("the title","the whole entire story.","entire story.",0,0,0)"""
+c.execute(q)
+#-------------------------------------------------------------------------------
 db.commit()
 db.close()
