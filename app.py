26a5d445423c1c259a03dfea7f6bfbbe0819bf2c
Anya Keller <anyakeller@betelgeuse.local>
2016-11-05 20:35:59 -0400

    Picked up login page by it's bootstraps and added limits to input

--- app.py
+++ app.py
@@ -29,7 +29,6 @@ def authOrCreate():
             session["username"]=username
             loginStatus = username + " logged in"
             return redirect( "/feed" )
-        
         elif statusNum == 2:
             loginStatus = "wrong password"
 
@@ -46,7 +45,7 @@ def authOrCreate():
         elif statusNum == 1:
             registerStatus = "passwords do not match"
         elif statusNum == 2:
-            registerStatus = username +" account Created"
+            registerStatus = username +" account created"
 
         return render_template("loginOrReg.html",status=registerStatus) #status is the login/creation messate 
     else:
