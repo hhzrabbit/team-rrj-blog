184ed8d13e09c13e7a45c575ddea4a91a0a048a4
Haley Zeng <hzeng@stuy.edu>
2016-11-05 23:06:08 -0400

    refactored some code

--- utils/initializeDb.py
+++ utils/initializeDb.py
@@ -3,6 +3,12 @@
 block comment describing the contents of this file
 '''
 import sqlite3   #enable control of an sqlite database
+import os
+
+keyFile = open("utils/key", "w")
+keyFile.write(os.urandom(32))
+keyFile.close()
+
 f = "database.db"
 
 db = sqlite3.connect(f) #open if f exists, otherwise create
@@ -17,14 +23,6 @@ c.execute(q)
 
 q="CREATE TABLE edit_logs (userId INTEGER, storyId INTEGER, time INTEGER)"
 c.execute(q)
-#add random stuff for testing
-#-------------------------------------------------------------------------------
 
-#---------------------------put stuff in tables---------------------------------
-q = """INSERT INTO users VALUES ("anya", "hashedpass", 0)"""
-c.execute(q)
-q = """INSERT INTO TABLE stories("the title","the whole entire story.","entire story.",0,0,0)"""
-c.execute(q)
-#-------------------------------------------------------------------------------
 db.commit()
 db.close()
